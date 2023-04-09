7e805cd97f9277b7147e5d99cdac471d865a66ca
ycq2021 <127408990+ycq2021@users.noreply.github.com>
2023-04-10 01:58:51 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -207,8 +207,53 @@ export const Chat: FC<Props> = memo(
                 >
                    获取OpenAI API密钥
                 </a>
-                   
-             
+              <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Bootstrap Modal Example</title>
+  <!-- 引入 Bootstrap 样式 -->
+  <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container py-3">
+    <h1>Bootstrap Modal Example</h1>
+    <p>点击按钮显示模态框</p>
+    <!-- 按钮 -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      显示模态框
+    </button>
+
+    <!-- 模态框 -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <!-- 这里放置您需要嵌入的网页或 URL 加载器 -->
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 引入 jQuery 和 Bootstrap JS -->
+  <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+              
+              
+              
             </div>
           </div>
         ) : modelError ? (
@@ -229,7 +274,7 @@ export const Chat: FC<Props> = memo(
                           <Spinner size="16px" className="mx-auto" />
                         </div>
                       ) : (
-                        'Chatbot UI'
+                        'ChatGpt AI'
                       )}
                     </div>
 
