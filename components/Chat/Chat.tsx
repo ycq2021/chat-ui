9c99588aa4ef5c25e0b5ae60c5e15d56d159e3e6
ycq2021 <127408990+ycq2021@users.noreply.github.com>
2023-04-11 14:04:25 +0800

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -175,8 +175,8 @@ export const Chat: FC<Props> = memo(
                 with their API.
               </div>
               <div className="mb-2">
-                别被山寨ChatGPT骗了,本站才是真的。话不多说如果你有官方密钥就可以在这里使用！\n
-                你在本站得来的密钥是永久性的记得保存好不要让别人知道！以免被多人使用封禁！！！
+                别被山寨ChatGPT骗了,本站才是真的.话不多说如果你有官方密钥就可以在这里使用！
+                密钥一般都是永久性的记得保存好不要让别人知道！以免被多人使用封禁！！！
               </div>
               <div className="mb-2">
                 It is <span className="italic">only</span> used to communicate
